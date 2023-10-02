b257e409476db911291b1b4ceb873952c01a0876
cozycole <tritchcole@gmail.com>
2023-10-01 21:17:29 -0700

    protects routes that can't be accessed unless logged in

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -43,3 +43,18 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAutheticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		// Also we want to make sure these pages that require
+		// authentication aren't stored in browser cache
+		w.Header().Add("Cache-Control", "no-store")
+
+		next.ServeHTTP(w, r)
+
+	})
+}
